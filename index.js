b7f5004199a818917615fc67a6e597e8a4f2c828
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-01-31 18:08:49 -0300

    Att index

--- index.js
+++ index.js
@@ -12,14 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
+app.use(cors());
 
 app.use("/", router);
 app.use("/post", routerPost);
