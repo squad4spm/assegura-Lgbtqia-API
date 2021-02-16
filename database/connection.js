c3948aa669e568861349dbe7c31c75ade29305ec
Elvis Brito <elvisbritodossantos61@gmail.com>
2021-02-15 21:03:27 -0300

    login

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 var knex = require("knex")({
   client: "mysql2",
   connection: {
-    host: "localhost",
-    user: "root",
-    port: "3306",
-    password: "",
+    host: "mysql741.umbler.com",
+    user: "recode_site",
+    port: "41890",
+    password: "reCode323##",
     database: "recode_site",
   },
 });
